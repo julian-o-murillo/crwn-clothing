fbdee089e252bbe4325ff414bf97e1353ab6e794
Julian Murillo <julianmurillo@Julians-MBP.attlocal.net>
2022-06-30 19:47:04 -0400

    restore from duplicate project

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,22 +1,22 @@
-import React, { Fragment } from "react";
-import { Outlet, Link } from "react-router-dom";
+import { Fragment } from 'react';
+import { Outlet, Link } from 'react-router-dom';
 
-import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 
-import "./navigation.styles.scss";
+import './navigation.styles.scss';
 
 const Navigation = () => {
   return (
     <Fragment>
       <div className='navigation'>
         <Link className='logo-container' to='/'>
-          <CrwnLogo>Logo</CrwnLogo>
+          <CrwnLogo className='logo' />
         </Link>
         <div className='nav-links-container'>
           <Link className='nav-link' to='/shop'>
             SHOP
           </Link>
-          <Link className='sign-in-link' to='/sign-in'>
+          <Link className='nav-link' to='/sign-in'>
             SIGN IN
           </Link>
         </div>
